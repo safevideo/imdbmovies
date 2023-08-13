7cbfafaef08e9c85ed0f8b58c49c6daf8586ac95
fatih <34196005+fcakyon@users.noreply.github.com>
2023-08-13 18:12:34 +0300

    change package name (#5)

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="moviedb",
-    version="0.1.0",
+    name="imdbmovies",
+    version="0.1.1",
     author="safevideo",
     description="IMDB API for Python",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/safevideo/moviedb",
+    url="https://github.com/safevideo/imdbmovies",
     packages=setuptools.find_packages(),
     zip_safe=False,
     install_requires=["beautifulsoup4"]
